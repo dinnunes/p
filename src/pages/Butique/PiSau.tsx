eb3c91389c7414db93e96c92d8b5026c71e3e5c0
Din <dinnuness@gmail.com>
2026-03-02 13:49:22 -0300

    Update PiSau.tsx

--- src/pages/Butique/PiSau.tsx
+++ src/pages/Butique/PiSau.tsx
@@ -4,7 +4,8 @@ export default function PiSau() {
   return (
     <ProductTemplate 
       category="Liivro" 
-      name="PiSau" 
+      name="PiSau"
+      image="/pisau.jpg"
       description="Existe um estado de existência que você ainda não acessou. Uma harmonia que se encontra no PiSau." 
       price="R$ 108" 
       link="https://pay.kiwify.com.br/giILQS6" 
