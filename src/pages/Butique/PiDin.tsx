258537d92c0b44f145f73062e062400221c48016
Din <dinnuness@gmail.com>
2026-03-02 12:39:16 -0300

    Update PiDin.tsx

--- src/pages/Butique/PiDin.tsx
+++ src/pages/Butique/PiDin.tsx
@@ -1,5 +1,13 @@
 import ProductTemplate from '../../components/ProductTemplate';
 
-export default function PiDin() {
-  return <ProductTemplate name="PiDin" description="Design estruturado e minimalista. Para quem busca a perfeição em cada detalhe." />;
+export default function PiTeni() {
+  return (
+    <ProductTemplate 
+      category="Liivro" 
+      name="PiDin" 
+      description="O dinheiro é a medida da sua liberdade. Se você não o controla, ele controla você. O mundo grita sobre “ficar rico”. O PiDin sussurra sobre ser livre." 
+      price="R$ 88" 
+      link="https://pay.kiwify.com.br/i9cgoaj" 
+    />
+  );
 }
