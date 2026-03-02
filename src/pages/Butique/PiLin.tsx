d84e05f14bc987a346cedb9d86613a16e80ad4d7
Din <dinnuness@gmail.com>
2026-03-02 12:42:11 -0300

    Update PiLin.tsx

--- src/pages/Butique/PiLin.tsx
+++ src/pages/Butique/PiLin.tsx
@@ -1,5 +1,15 @@
 import ProductTemplate from '../../components/ProductTemplate';
 
 export default function PiLin() {
-  return <ProductTemplate name="PiLin" description="Linhas puras e essenciais. A forma segue a função em sua expressão mais elegante." />;
+  return (
+    <ProductTemplate 
+      category="Liivro" 
+      name="PiLin" 
+      description="Estudar é um esforço consciente e cansativo. Adquirir é um processo invisível e inevitável. O PiLin não é sobre gramática, regras ou decoreba. 
+
+" 
+      price="R$ 88" 
+      link="https://pay.kiwify.com.br/9poKbxG" 
+    />
+  );
 }
