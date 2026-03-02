d50276c03ce4af56aaed15d953b4c0e7d4d703e7
Din <dinnuness@gmail.com>
2026-03-02 13:43:56 -0300

    Update PiLin.tsx

--- src/pages/Butique/PiLin.tsx
+++ src/pages/Butique/PiLin.tsx
@@ -4,10 +4,9 @@ export default function PiLin() {
   return (
     <ProductTemplate 
       category="Liivro" 
-      name="PiLin" 
-      description="Estudar é um esforço consciente e cansativo. Adquirir é um processo invisível e inevitável. O PiLin não é sobre gramática, regras ou decoreba. 
-
-" 
+      name="PiLin"
+      image="/pilin.jpg"
+      description="Estudar é um esforço consciente e cansativo. Adquirir é um processo invisível e inevitável. O PiLin não é sobre gramática, regras ou decoreba. " 
       price="R$ 88" 
       link="https://pay.kiwify.com.br/9poKbxG" 
     />
