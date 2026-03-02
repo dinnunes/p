704d4e8c009278cbca387773f7af5b365a7c7fa8
Din <dinnuness@gmail.com>
2026-03-02 12:54:49 -0300

    Update PiMor.tsx

--- src/pages/Butique/PiMor.tsx
+++ src/pages/Butique/PiMor.tsx
@@ -1,5 +1,14 @@
 import ProductTemplate from '../../components/ProductTemplate';
 
-export default function PiMor() {
-  return <ProductTemplate name="PiMor" description="Amor em cada detalhe. Uma peça criada com paixão e dedicação." />;
+export default function PiMor
+  () {
+  return (
+    <ProductTemplate 
+      category="Liivro" 
+      name="PiMor" 
+      description="A maioria das pessoas toca apenas a pele. Poucas têm a chave para tocar a alma. PiMor." 
+      price="R$ 98" 
+      link="https://pay.kiwify.com.br/yfcSxNI" 
+    />
+  );
 }
