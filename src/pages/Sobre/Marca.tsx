0c99a35fc16f0a45ce0c5bfb5abbc92b2740154c
Din <dinnuness@gmail.com>
2026-03-02 16:12:37 -0300

    Update Marca.tsx

--- src/pages/Sobre/Marca.tsx
+++ src/pages/Sobre/Marca.tsx
@@ -4,57 +4,61 @@ export default function Marca() {
   return (
     <PageTemplate title="Marca">
       <p>
-        <strong>
-          Lema  <br/>
-          ENIGMA SUBLIME...
-        </strong>
-      </p>
- <br/>
-
-      <p>
-        <strong>
-          Lema secundário  <br/>
-          UMA TRAGICOMÉDIA...
-        </strong>
-      </p>
- <br/>
-
-      <p>
-        <strong>
-          Lema especial  <br/>
-          ORDEM E PROGRESSO...
-        </strong>
-      </p>
- <br/>
+        <strong>
+          Lema <br/>
+          ENIGMA SUBLIME...
+        </strong>
+      </p>
+      <br/>
 
       <p>
         <strong>
-          Reticências é a forma curta.  <br/>
+          Lema secundário <br/>
+          UMA TRAGICOMÉDIA...
+        </strong>
+      </p>
+      <br/>
+
+      <p>
+        <strong>
+          Lema especial <br/>
+          ORDEM E PROGRESSO...
+        </strong>
+      </p>
+      <br/>
+
+      <p>
+        <strong>
+          Reticências é a forma curta. <br/>
           ...
         </strong>
       </p>
- <br/>
+      <br/>
+
       <p>
         A história simples. O nome Pitore se forma da simples ideia do Din de criar uma redução leve da palavra Pitoresco.
       </p>
-<br/>
+      <br/>
+
       <p>
         O “Pi” e o “▭” em qualquer tipografia e cor, são duas opções na forma de texto que você pode usar para se referir a Pitore.
       </p>
-<br/>      
+      <br/>      
+      
       <p>
         Os liivros tem a sua marca também, como você pode ver abaixo. PiTeni por exemplo, significa: Pitore de Entretenimento.
       </p>
-<br/>     
+      <br/>     
+      
       <p>
         Juntos, eles formam os “Pis“.
       </p>
-<br/>      
+      <br/>      
+      
       <p>
         PiTeni, PiDin, PiSau, PiSofia, PiLin, Pistilo e PiMor.
       </p>
-<br/>
-      </p>
+
     </PageTemplate>
   );
 }
