f988ff70e6ce4c6e8cff96abd761ba165bf6ca5d
Din <dinnuness@gmail.com>
2026-03-05 19:15:15 -0300

    Update PiSofia.tsx

--- src/pages/Butique/PiSofia.tsx
+++ src/pages/Butique/PiSofia.tsx
@@ -1,14 +1,19 @@
 import ProductTemplate from '../../components/ProductTemplate';
+import FAQ from '../../components/FAQ';
 
 export default function PiSofia() {
   return (
-    <ProductTemplate 
-      category="Liivro" 
-      name="PiSofia" 
-      image="/pisofia.jpg"
-      description="Você é o autor da sua história? Onde termina a sua natureza e começa a sua máscara? PiSofia." 
-      price="R$ 118" 
-      link="https://pay.kiwify.com.br/CqPNhIl" 
-    />
+    <>
+      <ProductTemplate 
+        category="Liivro" 
+        name="PiSofia"
+        image="/pisofia.jpg"
+        description="Você é o autor da sua história? Onde termina a sua natureza e começa a sua máscara? PiSofia." 
+        link="https://pay.kiwify.com.br/CqPNhIl" 
+      />
+      <div className="pb-20">
+        <FAQ />
+      </div>
+    </>
   );
 }
